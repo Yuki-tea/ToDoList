3f52a84a2f1e0c7fb34b2b620f238572c38bd05f
Yuki-tea <yuuki4168@gmail.com>
2026-05-12 18:24:58 +0900

    fix: エンドポイントを複数形に修正 (#33)

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
       </body>
     </html>
   );
