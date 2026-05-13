4fd8cb4d737fb62a8628bdb49bea70fae53c056b
Yuki-tea <yuuki4168@gmail.com>
2026-05-13 23:09:34 +0900

    feat: /focusの背景色を調整 (#40)

--- web/app/focus/Timer.tsx
+++ web/app/focus/Timer.tsx
@@ -4,9 +4,9 @@ type Props = {
   time: number;
 };
 
-export default function Timer(Props: { time: number }) {
-  const min = Math.floor(Props.time / 60);
-  const sec = Props.time % 60;
+export default function Timer({ time }: Props) {
+  const min = Math.floor(time / 60);
+  const sec = time % 60;
   return (
     <div className="text-6xl text-black font-mono mb-4">
       {min}:{sec.toString().padStart(2, "0")}
---
+++ web/app/focus/types.ts
@@ -0,0 +1,1 @@
+export type Mode = "Focus" | "Break" | "LongBreak";
