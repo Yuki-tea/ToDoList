1f179c24546ae65cda648524aeb4dd01fd493fcc
Yuki-tea <yuuki4168@gmail.com>
2026-05-18 10:18:51 +0900

    setup バックエンド側の証明書読み込み設定 (#48)

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'localhost.key')),
+    cert: fs.readFileSync(path.join(__dirname, '..', 'secrets', 'localhost.crt')),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on port: ${process.env.PORT}`);
 }
 bootstrap();
